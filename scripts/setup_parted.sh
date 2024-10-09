dcc9c659a53f2ee5aba99ab3bef94693942a4cbf
rhusiev <63148080+rhusiev@users.noreply.github.com>
2024-10-09 10:49:42 +0300

    ln instead of cp in setup_parted.sh

--- scripts/setup_parted.sh
+++ scripts/setup_parted.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-cp -r ./parted/ ./edk2-libc/AppPkg/Applications/Parted
-cp -r ./parted/AppPkg.dsc ./edk2-libc/AppPkg/AppPkg.dsc
+ln -sf ./parted/ ./edk2-libc/AppPkg/Applications/Parted
+ln -sf ./parted/AppPkg.dsc ./edk2-libc/AppPkg/AppPkg.dsc
