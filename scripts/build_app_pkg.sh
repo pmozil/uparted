65d6aaaac115a21e9929b7e4e77dd16bcbad89eb
rhusiev <63148080+rhusiev@users.noreply.github.com>
2024-10-14 15:46:22 +0300

    Remove DemoApp instead of Parted in setup_demo_app.sh

--- scripts/build_app_pkg.sh
+++ scripts/build_app_pkg.sh
@@ -2,7 +2,7 @@
 
 NUM_CPUS=$((`getconf _NPROCESSORS_ONLN` + 2))
 
-bash ./scripts/setup_parted.sh
-bash ./scripts/setup_demo_app.sh
-bash ./scripts/setup_app_pkg.sh
+. ./scripts/setup_parted.sh
+. ./scripts/setup_demo_app.sh
+. ./scripts/setup_app_pkg.sh
 build -n $NUM_CPUS -a X64 -t GCC -p ./edk2-libc/AppPkg/AppPkg.dsc
--- scripts/setup_demo_app.sh
+++ scripts/setup_demo_app.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 
-rm -rf ./edk2-libc/AppPkg/Applications/Parted
+rm -rf ./edk2-libc/AppPkg/Applications/DemoApp
 cp -r ./apps/demo_app/ ./edk2-libc/AppPkg/Applications/DemoApp
