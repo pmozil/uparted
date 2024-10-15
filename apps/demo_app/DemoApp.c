0964736cc6ecbc342a64df91dab7de0ba5805e64
Petro Mozil <mozil.petryk@gmail.com>
2024-10-15 11:57:51 +0300

    Add device size calculation
    Signed-off-by: Petro Mozil <mozil.petryk@gmail.com>

--- apps/demo_app/DemoApp.c
+++ apps/demo_app/DemoApp.c
@@ -6,25 +6,24 @@
 #include <Library/MemoryAllocationLib.h>
 #include <Library/UefiBootServicesTableLib.h>
 #include <Library/UefiLib.h>
+#include <Protocol/BlockIo.h>
 
 #include <stdio.h>
 #include <stdlib.h>
 
-EFI_STATUS GetPartitionNames(OUT EFI_DEVICE_PATH_PROTOCOL ***disks,
-                             OUT UINTN *partitionCount) {
+EFI_STATUS GetPartitionHandles(OUT EFI_HANDLE ***disks, OUT UINTN *n_handles) {
     EFI_STATUS status = EFI_SUCCESS;
 
-    if (disks == NULL || partitionCount == NULL) {
+    if (disks == NULL || n_handles == NULL) {
         puts("GetPartitionNames: invalid parameters");
         return EFI_INVALID_PARAMETER;
     }
-    *partitionCount = 0;
+    *n_handles = 0;
 
     UINTN handleCount;
     EFI_HANDLE *allHandles;
-    status =
-        gBS->LocateHandleBuffer(ByProtocol, &gEfiSimpleFileSystemProtocolGuid,
-                                NULL, &handleCount, &allHandles);
+    status = gBS->LocateHandleBuffer(ByProtocol, &gEfiBlockIoProtocolGuid, NULL,
+                                     &handleCount, &allHandles);
     if (EFI_ERROR(status))
         return status;
 
@@ -34,14 +33,14 @@ EFI_STATUS GetPartitionNames(OUT EFI_DEVICE_PATH_PROTOCOL ***disks,
         while (path != NULL && !IsDevicePathEndType(path)) {
             if (DevicePathType(path) == MEDIA_DEVICE_PATH &&
                 DevicePathSubType(path) == MEDIA_HARDDRIVE_DP) {
-                (*partitionCount)++;
+                (*n_handles)++;
+                break;
             }
             path = NextDevicePathNode(path);
         }
     }
 
-    *disks = (EFI_DEVICE_PATH_PROTOCOL **)malloc(
-        *partitionCount * sizeof(EFI_DEVICE_PATH_PROTOCOL *));
+    *disks = (EFI_HANDLE **)malloc(*n_handles * sizeof(EFI_HANDLE *));
     if (*disks == NULL) {
         puts("GetPartitionNames: no memory(");
         return EFI_OUT_OF_RESOURCES;
@@ -54,7 +53,8 @@ EFI_STATUS GetPartitionNames(OUT EFI_DEVICE_PATH_PROTOCOL ***disks,
         while (path != NULL && !IsDevicePathEndType(path)) {
             if (DevicePathType(path) == MEDIA_DEVICE_PATH &&
                 DevicePathSubType(path) == MEDIA_HARDDRIVE_DP) {
-                (*disks)[curIdx++] = path;
+                (*disks)[curIdx++] = allHandles[handleIdx];
+                break;
             }
             path = NextDevicePathNode(path);
         }
@@ -63,9 +63,10 @@ EFI_STATUS GetPartitionNames(OUT EFI_DEVICE_PATH_PROTOCOL ***disks,
 }
 
 int main(IN int Argc, IN char **Argv) {
-    EFI_DEVICE_PATH_PROTOCOL **disks = NULL;
+    EFI_HANDLE **disks = NULL;
     UINTN n_part = 0;
-    EFI_STATUS status = GetPartitionNames(&disks, &n_part);
+    EFI_STATUS status = GetPartitionHandles(&disks, &n_part);
+    EFI_BLOCK_IO_PROTOCOL *block_io;
 
     if (EFI_ERROR(status)) {
         puts("Error getting disks\n");
@@ -78,8 +79,22 @@ int main(IN int Argc, IN char **Argv) {
     }
 
     for (UINTN i = 0; i < n_part; i++) {
+        EFI_DEVICE_PATH_PROTOCOL *path = DevicePathFromHandle(disks[i]);
         Print(L"Device Path %d - %s\n", i,
-              ConvertDevicePathToText(disks[i], FALSE, TRUE));
+              ConvertDevicePathToText(path, FALSE, TRUE));
+
+        status = gBS->HandleProtocol(disks[i], &gEfiBlockIoProtocolGuid,
+                                     (VOID **)&block_io);
+        if (EFI_ERROR(status)) {
+            puts("Failed to open handle to disk");
+            continue;
+        }
+
+        if (block_io->Media->MediaPresent) {
+            UINT64 disk_size =
+                (block_io->Media->LastBlock + 1) * block_io->Media->BlockSize;
+            Print(L"Disk Size: %llu bytes\n", disk_size);
+        }
     }
 
     free(disks);
